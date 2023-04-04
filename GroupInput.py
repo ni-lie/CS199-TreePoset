27be5dd9ade52c5e7b8d26ec005cffe320c67ebc
ni-lie <cjay.coronel25@gmail.com>
2023-04-04 16:59:13 +0800

    feat: groups input linear orders based on their root

---
+++ GroupInput.py
@@ -0,0 +1,11 @@
+input = [1234, 1243, 4321, 3214, 2314, 2142, 1432]
+
+# convert the list of int to list of strings
+input = [str(item) for item in input]
+
+subgroup = [ [] for i in range(len(str(input[0])))]
+
+for i in range(len(input)):
+    subgroup[int(input[i][0])-1].append(int(input[i]))
+
+print(subgroup)
