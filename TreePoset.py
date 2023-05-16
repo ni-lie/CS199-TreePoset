072287b2857bb150b23eab50000006659ba8a42b
ni-lie <cjay.coronel25@gmail.com>
2023-05-16 16:22:33 +0800

    feat: added TreePoset algo based on pseudocode

--- TreePoset.py
+++ TreePoset.py
@@ -1,17 +1,40 @@
 import sys
 sys.path.append('Utils')
-from TreePoset_Utils_v2 import linear_order_to_binary_relation 
+from TreePoset_Utils_v2 import linear_order_to_binary_relation, binaryRelation, combinePoset 
 
-# import TreePoset_Utils_v2 as util
+def TreePoset(upsilon):
+    Pstar = upsilon
+    Ptree = []
+    isCombined = True
+    
+    while isCombined:
+        isCombined = False
 
-def TreePoset(P):
+        while len(Pstar) > 0:
+            poset1 = Pstar[0]
+            Pstar.pop(0)
+            hasPair = False
+            for i in range(len(Pstar)):
+                combinedposet = combinePoset(poset1, Pstar[i])
+                if combinedposet != None:
+                    Ptree.append(combinedposet)
+                    Pstar.pop(i)
+                    hasPair = True
+                    isCombined = True
+                    break
+            
+            if not hasPair:
+                Ptree.append(poset1)
+            
+        Pstar = Ptree.copy()
+        Ptree = []
 
+    return Ptree
 
 
 # CORRECT TEST CASES
 input_linear_order = [1234, 1243, 1324, 1342, 1423, 1432]
 input_linear_order = [1234, 1243, 1324, 1342, 1423]
-
 
 # WRONG TEST CASES
 # input_linear_order = [1234, 1324, 1423, 1432]       # returns a hammock poset (P1)
@@ -19,16 +42,19 @@ input_linear_order = [1234, 1243, 1324, 1342, 1423]
 # input_linear_order = ['12345', '12354', '12435', '13245', '13254', '13524', '14253', '14523', '14532', '15342', '15423', '15432']     # outputs 4 posets (optimal solution: 2 posets only)
 
 # current test case
-input_linear_order = ['12345', '12354', '12435', '13245', '13254', '13524', '14253', '14523', '14532', '15342', '15423', '15432']
+# input_linear_order = ['12345', '12354', '12435', '13245', '13254', '13524', '14253', '14523', '14532', '15342', '15423', '15432']
+
+
 
 input_linear_order = [str(item) for item in input_linear_order]
 input_linear_order.sort()
+Pstar = binaryRelation(input_linear_order)
 
-poset = binaryRelation(input_linear_order)
-
+for item in Pstar:
+    print(item)
 # run TreePoset
-result = TreePoset(poset)
+# result = TreePoset(poset)
 
-for i in range(len(result)):
-    print(f"P{str(i+1)}: {result[i]}")
+# for i in range(len(result)):
+#     print(f"P{str(i+1)}: {result[i]}")
 
