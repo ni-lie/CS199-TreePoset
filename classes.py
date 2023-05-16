99d3f13768de2a4b3bcc27e48188d55a29933c22
ni-lie <cjay.coronel25@gmail.com>
2023-05-16 16:53:40 +0800

    build: class for poset

---
+++ classes.py
@@ -0,0 +1,17 @@
+import networkx as nx
+class Poset:
+    def __init__(self, binaryRel, linearOrder):
+        self.binaryRel = binaryRel
+        self.linearOrder = linearOrder
+
+    def getLinearExtensions(self):
+        # Create a directed graph from the cover relation
+        G = nx.DiGraph()
+        for a, b in cover_relation:
+            G.add_edge(a, b)
+        
+        # Compute all possible topological sortings (i.e., linear extensions) of the graph
+        sortings = list(nx.all_topological_sorts(G))
+        
+        # Convert each sorting to a string and return the list of all sortings
+        return sorted([''.join(map(str, sorting)) for sorting in sortings])
