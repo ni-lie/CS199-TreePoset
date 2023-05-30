821d9b63b7edecb3eb5731a3e9d5ac7ddb22f42c
ni-lie <cjay.coronel25@gmail.com>
2023-05-30 08:24:35 +0800

    added the analysis_utils.py to this branch

--- Utils/Analysis_Utils.py
+++ Utils/Analysis_Utils.py
@@ -1,25 +1,25 @@
 import networkx as nx
+from TreePoset_Utils_v2 import get_linear_extensions
 
-def isTreePoset(P):
-    flag = True
-    #find number of vertices
-    n = 1
-    for pair in P:
-        if max(pair) > n:
-            n = max(pair)
-
+def isTreePoset(cover_relations):
+    n = len(cover_relations)+1
+    check_vertices = [False for x in range(n)] #array that checks if all vertices are included
     parent = [0 for x in range(n)] #array of number of parents of a vertex
-    
-    #store number of parents of each vertex by checking each pair in P
-    for (a,b) in P:
+    for (a,b) in cover_relations:
+        check_vertices[a-1] = True
+        check_vertices[b-1] = True
         parent[b-1] += 1
         
-    #check if a vertex has multiple parents,
+    #Check if the valid poset permutation is a tree poset
     for v in parent:
-        if (v > 1) or (0 not in parent): #if 0 not in parent, Poset possibly has a cycle
-            flag = False
-            break
-    return flag
+        if (v > 1) or (0 not in parent):
+            return False
+
+    #check if all edges are connected
+    if False not in check_vertices and nx.is_directed_acyclic_graph(nx.DiGraph(cover_relations)):
+        return True
+    else:
+        return False
 
 def areTreePosets(group_posets):
     flag = True
@@ -48,13 +48,21 @@ def binaryToCover(P,n):
             continue
         if len(coverRelations) == n - 1:
             break
+        transitive = False
         for w in range(1, n+1):
             if w == u or w == v:
                 continue
             else:
                 if (u,w) in P and (w,v) in P:
+                    transitive = True
                     break
-                else:
-                    coverRelations.append((u,v))
-                    break
+        if not transitive:
+            coverRelations.append((u,v))
     return sorted(coverRelations)
+
+def covered(group_posets):
+    coveredlinearorders = []
+    for poset in group_posets:
+        coveredlinearorders += get_linear_extensions(poset)
+    coveredlinearorders = [int(x) for x in list(set(coveredlinearorders))]
+    return sorted(coveredlinearorders)
