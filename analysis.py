b53c6e15bab651013ad8033c245e43f42f0bbad5
ni-lie <cjay.coronel25@gmail.com>
2023-05-30 22:47:05 +0800

    resolved some conflicts

--- analysis.py
+++ analysis.py
@@ -48,7 +48,7 @@ with open(f'optsol/trees/{args[1]}treesoptsol.txt', 'r') as optimal_file, open(f
     for line in HeuristicOutput_file:
         if line[:7] == "Input: ":
             continue
-        elif line[:2] == "P1":
+        elif line[:4] == "P1: ":
             #first poset is added to the list of heuristic solutions
             start = line.index('[')
             lst = literal_eval(line[start:])
@@ -84,6 +84,8 @@ with open(f'optsol/trees/{args[1]}treesoptsol.txt', 'r') as optimal_file, open(f
     len_inputs = len(inputs)
     correct = 0
     optimal = 0
+    not_optimal = []
+    performanceRatio = 0
     for (input, cost, O_sol, H_sol) in zip(inputs, optcost, optsol, heuristicsol):
         output.write("Input: "+ str(input)+"\n")
         output.write("Optimal Solution: "+ str(O_sol)+"\n")
@@ -92,18 +94,32 @@ with open(f'optsol/trees/{args[1]}treesoptsol.txt', 'r') as optimal_file, open(f
             output.write("Analysis: CORRECT - OPTIMAL\n")
             correct += 1
             optimal += 1
+            performanceRatio +=1
         elif areTreePosets(H_sol) and isAllConnected(H_sol, n) and len(H_sol) > cost and covered(H_sol) == input:
-            output.write("Analysis: CORRECT - NOT OPTIMAL\n")
+            diff_cost = len(H_sol) - cost
+            not_optimal.append(diff_cost)
             correct += 1
+            performanceRatio += len(H_sol) / cost
+            output.write("Analysis: CORRECT - NOT OPTIMAL\n")
+            output.write("Heuristic_Cost - Optimal_Cost: "+str(diff_cost)+"\n")
         else:
             output.write("Analysis: INCORRECT - NOT OPTIMAL\n")
             output.write("Output: "+ str(covered(H_sol))+"\n")
         output.write("\n")
 
+    if len(not_optimal) == 0:
+        average_diff_not_optimal = 0
+        max_not_optimal = 0
+    else:
+        average_diff_not_optimal = sum(not_optimal)/len(not_optimal)
+        max_not_optimal = max(not_optimal)
     output.write("Summary\n")
     output.write("Total Number of Inputs: "+str(len_inputs)+"\n")
     output.write("Total Number of Correct Heuristic Solutions: "+str(correct)+"\n")
     output.write("Total Number of Optimal Heuristic Solutions: "+str(optimal)+"\n")
+    output.write("Average difference in Heuristic Cost and Optimal Cost: "+str(average_diff_not_optimal)+"\n")
+    output.write("Maximum difference in Heuristic Cost and Optimal Cost: "+str(max_not_optimal)+"\n")
+    output.write("Performance Approximation Ratio: "+str((performanceRatio/len_inputs))+"\n")
     output.close
 
     print("FINISHED ANALYSING HEURISTIC")
